8f5b29958f06d3769c248a5e89f9bf5717765a4d
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2022-10-19 09:04:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZbrojIProsjek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaC klasaC = new KlasaC();
+            
+            Console.WriteLine(klasaC.Upis());
+            Console.WriteLine(klasaC.IspisZbroj());
+            Console.WriteLine(klasaC.IspisProsjek());
+            Console.ReadKey();
+        }
+        class KlasaC
+        {
+            int zbroj;
+            int[] brojevi = new int[5];
+
+            public KlasaC()
+            {
+              
+            }
+            public KlasaC(int[] brojevi)
+            {
+                this.brojevi = brojevi;
+            }
+
+            public int[] Upis()
+            {
+                Console.WriteLine("Upisi te 5 brojeva");
+                for(int i = 0; i < 5; i++)
+                {
+                    this.brojevi[i]=Convert.ToInt32(Console.ReadLine());
+                }
+                return (brojevi);
+            }
+            public string IspisZbroj()
+            {
+                string ispis;
+                for(int i = 0; i < 5; i++)
+                {
+                    zbroj+= brojevi[i];
+                }
+                ispis = ("Zbroj brojeva je: " + zbroj);
+                return ispis;
+            }
+            public string IspisProsjek()
+            {
+                string ispis;
+                double prosjek;
+                prosjek = zbroj / 5;
+                ispis = ("Prosjek zbroja je: " + prosjek);
+                return ispis;
+            }
+
+        }
+    }
+}
